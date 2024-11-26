a6de8c2a78af37fb2fc55dffa443ce8a8c909bb0
SamSyamsudin <syams.69@gmail.com>
2024-11-26 16:01:38 +0700

    Update print_request.php

--- application/views/Pembelian/print_request.php
+++ application/views/Pembelian/print_request.php
@@ -10,10 +10,7 @@ require_once(APPPATH.'libraries/MPDF57/mpdf.php');
 	
 	$data_iden	= $this->db->get('identitas')->result();
     $owner      = $datapo->id_supplier;
-	$data_bank	= $this->db->get_where('supplierX', array('id_supplier =' => $owner))->result();
-
-	print_r($data_bank);
-	exit;
+	$data_bank	= $this->db->get_where('supplier', array('id_supplier =' => $owner))->result();
 
 	echo "<table class='gridtable2' border='0' width='100%' cellpadding='2'>";
 		echo "<tr>";
