ec701a5263fe679efca6d6a1ddea542b151f3af2
SamSyamsudin <syams.69@gmail.com>
2024-12-16 09:37:09 +0700

    Update expense.php

--- application/views/Expense/expense.php
+++ application/views/Expense/expense.php
@@ -53,12 +53,12 @@ $this->load->view('include/side_menu');
 					<a class="btn btn-warning btn-sm view" href="javascript:void(0)" title="View" onclick="data_view('<?=$record->id?>')"><i class="fa fa-eye"></i></a>
                 <?php }
                 if($akses_menu['update']=='1'){
-                    if ($record->status=="0" || $record->status=="9") {?>
+                    if ($record->status==0) {?>
                     <a class="btn btn-success btn-sm edit" href="javascript:void(0)" title="Edit" onclick="data_edit('<?=$record->id?>')"><i class="fa fa-edit"></i></a>
                     <?php }
                 }
                 if($akses_menu['delete']=='1'){
-                    if ($record->status=="0" || $record->status=="9") {?>
+                    if ($record->status==0) {?>
                     <a class="btn btn-danger btn-sm delete" href="javascript:void(0)" title="Hapus" onclick="data_delete('<?=$record->id?>')"><i class="fa fa-trash"></i></a>
                     <?php }
                     } ?>
