d77b36f1a4f69953dce53c871f7f8ee2abee04da
arwant2411 <suparwanto4141@gmail.com>
2024-12-17 08:40:44 +0700

    status deputi po

--- application/models/Purchase_order_model.php
+++ application/models/Purchase_order_model.php
@@ -2405,17 +2405,18 @@ class Purchase_order_model extends CI_Model {
 			
 			$nestedData[]	= "<div align='left'><span class='badge' style='background-color: ".color_status_purchase($row['sts_ajuan'])['color']."'>".color_status_purchase($row['sts_ajuan'])['status']."</span></div>";
 				$ajukan	= "";
+				$hasil	= "";
+				if(($row['sts_ajuan']=='APV' OR $row['sts_ajuan']=='CLS') AND $row['sts_process']=='Y'){
+					$hasil	= "&nbsp;<button type='button' class='btn btn-sm btn-success hasil_ajukan' title='Hasil Perbandingan' data-no_rfq='".$row['no_rfq']."'><i class='fa fa-eye'></i></button>";
+				}
 				if($row['sts_ajuan']=='APV' AND $row['sts_process']=='Y'){
-
 					if($Arr_Akses['approve']=='1'){
 						$ajukan	= "&nbsp;<button type='button' class='btn btn-sm btn-info approved' title='Approve' data-no_rfq='".$row['no_rfq']."'><i class='fa fa-check'></i></button>";
 					}
 				}
-				if(($row['sts_ajuan']=='APV' OR $row['sts_ajuan']=='CLS') AND $row['sts_process']=='Y'){
-					$ajukan	= "&nbsp;<button type='button' class='btn btn-sm btn-success hasil_ajukan' title='Hasil Perbandingan' data-no_rfq='".$row['no_rfq']."'><i class='fa fa-eye'></i></button>";
-				}
 			$nestedData[]	= "<div align='left'>
                                     <button type='button' class='btn btn-sm btn-primary detailMat' title='Total Material Purchase' data-no_rfq='".$row['no_rfq']."' data-status='".$row['sts_ajuan']."'><i class='fa fa-eye'></i></button>
+                                   ".$hasil."
                                    ".$ajukan."
 									</div>";
 			$data[] = $nestedData;
