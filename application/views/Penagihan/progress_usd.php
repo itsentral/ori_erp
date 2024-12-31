0989fa683c097ae60469b130196f647f235723e4
SamSyamsudin <syams.69@gmail.com>
2024-12-31 15:54:24 +0700

    Update progress_usd.php

--- application/views/Penagihan/progress_usd.php
+++ application/views/Penagihan/progress_usd.php
@@ -137,7 +137,9 @@
 			$spesifikasi='';
 			$get_bqHead = $this->db->get_where('tanki.bq_detail_header', array('no_ipp'=>$valx['no_ipp'],'category'=>'frp'))->result();
 			if(!empty($get_bqHead))  {
-				$spesifikasi=number_format($get_bqHead[0]['length']).' X '.number_format($get_bqHead[0]['wide']).' X '.number_format($get_bqHead[0]['high']);
+				$spesifikasi=number_format($get_bqHead[0]['length']).' X '
+				.number_format($get_bqHead[0]['wide']).' X '
+				.number_format($get_bqHead[0]['high']);
 			}
 			?>
 			<tr id='tr_<?= $numb;?>' >
