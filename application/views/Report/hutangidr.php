5f29403f6ccf5f6b6e58a1b0dca64cc393d324b8
SamSyamsudin <syams.69@gmail.com>
2025-01-02 17:15:48 +0700

    Update hutangidr.php

--- application/views/Report/hutangidr.php
+++ application/views/Report/hutangidr.php
@@ -60,7 +60,7 @@ $this->load->view('include/side_menu');
 			}], 
 			"sPaginationType": "simple_numbers",
 			"iDisplayLength": 10,
-			"aLengthMenu": [[10, 20, 50, 100, 150, all], [10, 20, 50, 100, 150, all]],
+			"aLengthMenu": [[10, 20, 50, 100, 150, 'All'], [10, 20, 50, 100, 150, 'All']],
 			"ajax":{
 				url : base_url + active_controller+'/server_side_hutangidr',
 				type: "post",
