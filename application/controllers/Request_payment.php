802fb9604ebd265f9ca735cf26499761b001582b
SamSyamsudin <syams.69@gmail.com>
2025-03-12 11:41:49 +0700

    Update Request_payment.php

--- application/controllers/Request_payment.php
+++ application/controllers/Request_payment.php
@@ -681,7 +681,7 @@ class Request_payment extends CI_Controller {
 					if ($tipe[$keys] == 'kasbon') {
 						$rec = $this->db->query("select * from " . DBACC . ".master_oto_jurnal_detail where kode_master_jurnal='" . $jenis_jurnal . "' and menu='" . $tipe[$keys] . "'")->row();
 					
-							$coa_kasbon = $this->db->query("select * from tr_kasbon where no_doc'" . $no_doc[$keys] . "'")->row();
+							$coa_kasbon = $this->db->query("select * from tr_kasbon where no_doc='" . $no_doc[$keys] . "'")->row();
 							$det_Jurnaltes1[] = array(
 								'nomor' => $nomor_jurnal, 'tanggal' => $payment_date, 'tipe' => 'BUK', 'no_perkiraan' => $coa_kasbon->coa, 'keterangan' => $keterangan[$keys], 'no_request' => $no_doc[$keys], 'debet' => $bank_nilai[$keys], 'kredit' => 0, 'nilai_valas_debet' => 0, 'nilai_valas_kredit' => 0, 'no_reff' =>  $no_doc[$keys], 'jenis_jurnal' => $jenis_jurnal, 'nocust' => $nama[$keys], 'stspos' => '1'
 							);
