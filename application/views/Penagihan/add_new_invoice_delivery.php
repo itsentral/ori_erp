5356916d9de2de69e8f40e36022b32b9f77b43e0
SamSyamsudin <syams.69@gmail.com>
2025-03-27 13:40:08 +0700

    Update add_new_invoice_delivery.php

--- application/views/Penagihan/add_new_invoice_delivery.php
+++ application/views/Penagihan/add_new_invoice_delivery.php
@@ -1049,7 +1049,6 @@ if(isset($approval)){
 						+ getNum(result4_hidden1)
 						+ getNum(result5_hidden1)
 						+ getNum(result6_hidden1)
-						+ getNum(ppn_hidden)
 						+ getNum(result7_hidden1)
 						+ getNum(result8_hidden1)
 						- getNum(diskon_hidden)
