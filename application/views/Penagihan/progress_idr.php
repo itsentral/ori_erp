9886ea8a9b6a87f2ac2b368da255d6a2bbaa477b
SamSyamsudin <syams.69@gmail.com>
2025-05-07 09:44:11 +0700

    Update progress_idr.php

--- application/views/Penagihan/progress_idr.php
+++ application/views/Penagihan/progress_idr.php
@@ -660,7 +660,9 @@
 					$down_payment2=0;
 				}
 				?>
-				<input type="text" class="form-control grand_total text-right input-sm divide" id="grand_total" name="grand_total" value="<?php echo set_value('grand_total', isset($grand_total) ? $grand_total : '0'); ?>" tabindex="-1">				
+				<input type="hidden" class="form-control grand_total text-right input-sm divide" id="grand_total" name="grand_total" value="<?php echo set_value('grand_total', isset($grand_total) ? $grand_total : '0'); ?>" tabindex="-1">
+				<input type="text" class="form-control grand_total text-right input-sm divide" id="dpp_grand_total" name="dpp_grand_total" value="<?php echo set_value('grand_total', isset($grand_total) ? $grand_total : '0'); ?>" tabindex="-1">				
+					
 			</td>
 		</tr>
 		<tr class='HeaderHr'>
