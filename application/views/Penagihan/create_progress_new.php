f9da9e07129a58710aa711effd808ca1f86c84fd
SamSyamsudin <syams.69@gmail.com>
2025-05-07 10:33:17 +0700

    Update create_progress_new.php

--- application/views/Penagihan/create_progress_new.php
+++ application/views/Penagihan/create_progress_new.php
@@ -574,7 +574,7 @@ if(isset($approval)){
 		});
 
 		$(document).on('change', '#persen_retensi', function(){
-			var grand_total_hidden	= $("#grand_total").val();
+			var grand_total_hidden	= $("#dpp_grand_total").val();
 			var datRetensi         	= $('#persen_retensi').val();
 			totalret=(parseFloat(grand_total_hidden)*parseFloat(datRetensi/100)).toFixed(2);
 			$('.potongan_retensi').val(num2(totalret));
@@ -585,7 +585,10 @@ if(isset($approval)){
 			var grand_total_hidden	= $("#dpp_grand_total").val();
 			var datRetensi         	= $('#persen_retensi2').val();
 			Retensi2=(parseFloat(grand_total_hidden)*parseFloat(datRetensi/100)).toFixed(2);
+            Grand_total2= parseFloat(grand_total_hidden)-parseFloat(Retensi2);
+
 			$('.potongan_retensi2').val(number_format(Retensi2,2));
+			$('.grand_total_dpp').val(number_format(Grand_total2,2));
 			ppn();
 		});
 
