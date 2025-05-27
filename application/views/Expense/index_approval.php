0bf9a6d69468284c7a82c1096a085cf468e849d2
SamSyamsudin <syams.69@gmail.com>
2025-05-27 17:07:55 +0700

    Update index_approval.php

--- application/views/Expense/index_approval.php
+++ application/views/Expense/index_approval.php
@@ -11,7 +11,7 @@ $this->load->view('include/side_menu');
 	</div>
 	<!-- /.box-header -->
 	<div class="box-body">
-        <table id="mytabledata" class="table table-bordered table-striped">
+        <table id="example1" class="table table-bordered table-striped">
             <thead>
             <tr>
 				<th width="5">#</th>
