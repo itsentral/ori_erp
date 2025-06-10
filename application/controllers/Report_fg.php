ef2199b1ebc6879346b3b9825124f1ef8cb33e42
SamSyamsudin <syams.69@gmail.com>
2025-06-10 15:10:02 +0700

    Update Report_fg.php

--- application/controllers/Report_fg.php
+++ application/controllers/Report_fg.php
@@ -545,8 +545,13 @@ class Report_fg extends CI_Controller {
             
 			$idtrans = $row['id_trans'];
 			$out = $this->db->query("SELECT a.qty as qty_out FROM data_erp_in_transit a WHERE jenis='in' AND id_trans=$idtrans GROUP BY id_trans")->row();
-            $qty = $row['total_qty'];
-			$qty_out = (!empty($out->qty_out))?$out->qty_out:0;				
+            
+			if(!empty($out)){
+			$qty_out = 	$out->qty_out;
+			}else{
+			$qty_out = 0;	
+			}		
+			$qty = $row['total_qty'];
 			$qty_sisa = $qty - $qty_out;
 			$nestedData 	= array();
 			$nestedData[]	= "<div align='center'>".$nomor."</div>";
