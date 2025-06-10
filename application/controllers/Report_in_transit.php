c4a7f5db5f66179be456920e7c54a404135a062f
SamSyamsudin <syams.69@gmail.com>
2025-06-10 17:10:33 +0700

    Update Report_in_transit.php

--- application/controllers/Report_in_transit.php
+++ application/controllers/Report_in_transit.php
@@ -376,8 +376,9 @@ class Report_in_transit extends CI_Controller {
             }
             
 			$idtrans = $row['id_trans'];
+			if(!empty($idtrans)){
 			$out = $this->db->query("SELECT a.qty as qty_out FROM data_erp_in_customer a WHERE jenis='in' AND id_trans=$idtrans GROUP BY id_trans")->row();
-            
+			}
 			if(!empty($out)){
 			$qty_out = 	$out->qty_out;
 			}else{
