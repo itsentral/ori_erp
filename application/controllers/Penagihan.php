e275e06826603e88798a39e2588464eed5fcfc73
SamSyamsudin <syams.69@gmail.com>
2025-07-28 17:56:52 +0700

    Update Penagihan.php

--- application/controllers/Penagihan.php
+++ application/controllers/Penagihan.php
@@ -6168,9 +6168,11 @@ if($base_cur=='USD'){
 				$material	= $this->db->select('*, unit satuan, qty as qty_delivery,qty_sisa as qty_inv, nm_material as product, product_cust as customer_item')->where('id_penagihan',$id)->get_where('penagihan_detail',array('kategori_detail'=>'MATERIAL'))->result_array();
 				$list_top	= $this->db->get_where('list_help', array('group_by'=>'top invoice'))->result_array();
 
-				$get_kurs	= $this->db->select(' (kurs_jual) AS kurs,  (progress_persen) AS uang_muka_persen,  0 AS uang_muka_persen2')->where('id',$id)->get('penagihan')->result();
 				$get_tagih	= $this->db->order_by('id','ASC')->get_where('penagihan',array('no_po'=>$penagihan[0]->no_po,'type'=>'uang muka'))->result();
+				$get_kurs  = $this->db->query("select persen_um as uang_muka_persen,kurs_um as kurs,sisa_um AS sisa_um,sisa_um_idr AS sisa_um_idr from tr_kartu_po_customer where nomor_po ='".$penagihan[0]->no_po."'")->result();
+				$sisa_um   = $get_kurs[0]->sisa_um;
 				$uang_muka_persen = $get_kurs[0]->uang_muka_persen;
+				$sisa_um_idr   = $get_kurs[0]->sisa_um_idr;
 				if($base_cur=='USD'){
 					$down_payment = (!empty($get_tagih))?$get_tagih[0]->grand_total:0;
 				}else{
@@ -6255,8 +6257,10 @@ if($base_cur=='USD'){
 				'kurs'			=> $get_kurs[0]->kurs,
 				'uang_muka_persen'	=> $get_kurs[0]->uang_muka_persen,
 				'uang_muka_persen2'	=> 0,
-				'down_payment'	=> 0,
-				'down_payment2'	=> 0,
+				'down_payment'	=> $down_payment,
+				'sisa_um'	    => $sisa_um,
+				'sisa_um_idr'	    => $sisa_um_idr,
+				'down_payment2'	=> $down_payment2,
 				'id'			=> $id,
 				'approval'		=> $approval
 			);
