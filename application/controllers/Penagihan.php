1fb2de5a6dc97fe4284d2198a003079ea5d9c8ef
SamSyamsudin <syams.69@gmail.com>
2025-07-30 08:51:58 +0700

    Update Penagihan.php

--- application/controllers/Penagihan.php
+++ application/controllers/Penagihan.php
@@ -6570,7 +6570,7 @@ if($base_cur=='USD'){
 			$getDetail	= $this->db->query("select a.*, a.qty as qty_total, (a.qty-a.qty_inv) as qty_inv, c.qty as qty_delivery, c.cogs, c.sts_do from billing_so_product a join so_bf_detail_header b on a.id_milik=b.id_milik join ( select sum(x.qty) as qty, sum(x.cogs) as cogs, x.no_ipp,x.id_product, CONCAT('BQ-',x.no_ipp) as id_bq, x.id_penagihan, y.id_milik, x.sts_do	 from penagihan_product_temp x join so_detail_header y on x.id_milik=y.id WHERE
 			x.id_penagihan='".$id."' group by x.no_ipp,x.id_product,y.id_milik) c on b.id=c.id_milik and b.id_bq=c.id_bq and a.no_ipp=c.no_ipp")->result_array();
 			
-			$getDetailcut	= $this->db->query("select a.* FROM penagihan_product_temp WHERE sts_do='cut non produksi'")->result_array();
+			$getDetailcut	= $this->db->query("select a.* FROM penagihan_product_temp a WHERE a.sts_do='cut non produksi'")->result_array();
 			
 			
 			if($ada_data_bf!=''){
