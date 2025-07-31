34ae429953fc996649660e608304036ce6591484
SamSyamsudin <syams.69@gmail.com>
2025-07-31 11:25:22 +0700

    Update add_new_invoice_delivery.php

--- application/views/Penagihan/add_new_invoice_delivery.php
+++ application/views/Penagihan/add_new_invoice_delivery.php
@@ -61,10 +61,7 @@ $this->load->view('include/side_menu');
 				<label class='label-control col-sm-2'><b>Kurs <span class='text-red'>*</span></b></label>
 				<div class='col-sm-2'>
 					<input type='text' name='kurs' id='kurs' class='form-control input-md' value='<?=number_format($kurs,2);?>' data-decimal='.' data-thousand='' data-precision='0' data-allow-zero=''>
-					<?php print_r($getHeader[0]->no_ipp);
-					exit;
-
-					if(SUBSTR($getHeader[0]->no_ipp,0,4)) != IPPT ){ ?>
+					<?php if(strpos($getHeader[0]->no_ipp, "IPPT") !== false){ ?>
 					<input type="hidden" id='wilayah' name="wilayah" class="form-control input-sm" value="<?= get_name('so_number','wilayah','id_bq', "BQ-".$getHeader[0]->no_ipp);?>">
 				    <?php } else {?>
 						<input type="hidden" id='wilayah' name="wilayah" class="form-control input-sm" value="<?= get_name('so_number','wilayah','id_bq', "BQ-".$getHeader[0]->no_ipp);?>">
