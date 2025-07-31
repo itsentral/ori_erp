09eec13f7443716f1bb937583a0afbca3364c6aa
SamSyamsudin <syams.69@gmail.com>
2025-07-31 10:24:50 +0700

    Update Penagihan.php

--- application/controllers/Penagihan.php
+++ application/controllers/Penagihan.php
@@ -4167,7 +4167,7 @@ else
 			if($kodejurnal1		= 'JV061';){
             
 			$this->db->query("INSERT INTO data_erp_in_customer (tanggal,keterangan,no_so,product,no_spk,kode_trans,id_pro_det,qty,nilai_unit,created_by,created_date,id_trans,id_pro,qty_ke,kode_delivery,jenis,id_material,nm_material,qty_mat,cost_book,gudang,kode_spool)			
-			SELECT tanggal,keterangan,no_so,product,no_spk,kode_trans,id_pro_det,qty,nilai_unit,created_by,created_date,id_trans,id_pro,qty_ke,kode_delivery,'out',id_material,nm_material,qty_mat,cost_book,gudang,kode_spool FROM data_erp_in_customer WHERE kode_delivery IN ".$kode_delivery."");
+			SELECT ".$tgl.",keterangan,no_so,product,no_spk,kode_trans,id_pro_det,qty,nilai_unit,created_by,created_date,id_trans,id_pro,qty_ke,kode_delivery,'out',id_material,nm_material,qty_mat,cost_book,gudang,kode_spool FROM data_erp_in_customer WHERE kode_delivery IN ".$kode_delivery."");
 			}
 			
 
