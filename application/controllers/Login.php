515ec12c07ca324f28afea9eb43cc2254da0aa3a
SamSyamsudin <syams.69@gmail.com>
2025-08-19 14:00:32 +0700

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -32,7 +32,7 @@ class Login extends CI_Controller {
 
             $urlVeryfy    = "https://www.google.com/recaptcha/api/siteverify?secret=" . urlencode($this->secret_key) . "&response=" . urlencode($token);
             $resGoogle     = json_decode(file_get_contents($urlVeryfy));
-            print_r($resGoogle);
+            print_r($token);
             exit;
 
             if (!$resGoogle->success) {
