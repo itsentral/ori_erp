f61d14a2dc3fc5a6be3307a17c6fbbe8794ede05
SamSyamsudin <syams.69@gmail.com>
2025-09-29 11:54:04 +0700

    Update Penagihan.php

--- application/controllers/Penagihan.php
+++ application/controllers/Penagihan.php
@@ -4253,12 +4253,12 @@ else
 			$db2->insert('javh',$dataJVhead);
 			$db2->insert_batch('jurnal',$det_Jurnaltes1);
 			if($kodejurnal1		= 'JV061'){
-            
+            $tgl1       		= $gethd->tgl_invoice;
 			$this->db->query("INSERT INTO data_erp_in_customer (tanggal,keterangan,no_so,product,no_spk,kode_trans,id_pro_det,qty,nilai_unit,created_by,created_date,id_trans,id_pro,qty_ke,kode_delivery,jenis,id_material,nm_material,qty_mat,cost_book,gudang,kode_spool)			
-			SELECT ".$tgl.",keterangan,no_so,product,no_spk,kode_trans,id_pro_det,qty,nilai_unit,created_by,created_date,id_trans,id_pro,qty_ke,kode_delivery,'out',id_material,nm_material,qty_mat,cost_book,gudang,kode_spool FROM data_erp_in_customer WHERE kode_delivery IN (".$kode_delivery.")");
+			SELECT ".$tgl1.",keterangan,no_so,product,no_spk,kode_trans,id_pro_det,qty,nilai_unit,created_by,created_date,id_trans,id_pro,qty_ke,kode_delivery,'out',id_material,nm_material,qty_mat,cost_book,gudang,kode_spool FROM data_erp_in_customer WHERE kode_delivery IN (".$kode_delivery.")");
 
 			$this->db->query("INSERT INTO data_erp_cogs (tanggal,keterangan,no_so,product,no_spk,kode_trans,id_pro_det,qty,nilai_unit,created_by,created_date,id_trans,id_pro,qty_ke,kode_delivery,jenis,id_material,nm_material,qty_mat,cost_book,gudang,kode_spool)			
-			SELECT ".$tgl.",keterangan,no_so,product,no_spk,kode_trans,id_pro_det,qty,nilai_unit,created_by,created_date,id_trans,id_pro,qty_ke,kode_delivery,'in',id_material,nm_material,qty_mat,cost_book,gudang,kode_spool FROM data_erp_in_customer WHERE kode_delivery IN (".$kode_delivery.")");
+			SELECT ".$tgl1.",'In customer-COGS',no_so,product,no_spk,kode_trans,id_pro_det,qty,nilai_unit,created_by,created_date,id_trans,id_pro,qty_ke,kode_delivery,'in',id_material,nm_material,qty_mat,cost_book,gudang,kode_spool FROM data_erp_in_customer WHERE kode_delivery IN (".$kode_delivery.")");
 			
 		
 		}
