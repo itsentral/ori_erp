7baa09b209d7fa71c2c839382185e9028f247983
SamSyamsudin <syams.69@gmail.com>
2025-10-24 09:35:39 +0700

    Update Rutin_model.php

--- application/models/Rutin_model.php
+++ application/models/Rutin_model.php
@@ -1650,8 +1650,7 @@ class Rutin_model extends CI_Model {
 				b.spec,
 				b.id_material
 			FROM
-			    (SELECT @row:=0) r, 
-				rutin_planning_header a
+			  	rutin_planning_header a
 				LEFT JOIN rutin_planning_detail b ON a.no_pengajuan=b.no_pengajuan
 				
 		    WHERE 1=1 ".$where_range."
