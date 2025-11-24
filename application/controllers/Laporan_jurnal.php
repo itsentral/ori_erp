1f8722aa760038b94badd7b46015281f50582297
SamSyamsudin <syams.69@gmail.com>
2025-11-24 11:12:48 +0700

    Update Laporan_jurnal.php

--- application/controllers/Laporan_jurnal.php
+++ application/controllers/Laporan_jurnal.php
@@ -51,7 +51,7 @@ class Laporan_jurnal extends CI_Controller {
 
     private function export_excel($results, $dari, $sampai)
     {
-    require FCPATH . 'vendor/autoload.php';
+    $this->load->library("PHPExcel");
 
     $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
     $sheet = $spreadsheet->getActiveSheet();
@@ -100,5 +100,206 @@ class Laporan_jurnal extends CI_Controller {
     $writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
     $writer->save("php://output");
     }
+
+
+    public function excel_report_subgudang3(){
+		//membuat objek PHPExcel
+		set_time_limit(0);
+		ini_set('memory_limit','1024M');
+		$tgl_awal		= $this->uri->segment(3);
+		$tgl_akhir		= $this->uri->segment(4);
+
+		$this->load->library("PHPExcel");
+		$objPHPExcel	= new PHPExcel();
+
+		$whiteCenterBold    = whiteCenterBold();
+		$whiteRightBold    	= whiteRightBold();
+		$whiteCenter    	= whiteCenter();
+		$mainTitle    		= mainTitle();
+		$tableHeader    	= tableHeader();
+		$tableBodyCenter    = tableBodyCenter();
+		$tableBodyLeft    	= tableBodyLeft();
+		$tableBodyRight    	= tableBodyRight();
+
+		$Arr_Bulan	= array(1=>'Jan','Feb','Mar','Apr','Mei','Jun','Jul','Aug','Sep','Oct','Nov','Dec');
+		$sheet 		= $objPHPExcel->getActiveSheet();
+
+        $WHERE_DATE = "AND a.tanggal LIKE '".date('Y')."-".date('m')."%' ";
+		if($tgl_awal != '0'){
+			$WHERE_DATE = "AND (DATE( a.tanggal ) BETWEEN '".$tgl_awal."' AND '".$tgl_akhir."' )";
+		}
+
+        $sql = "SELECT a.* FROM data_erp_in_customer a WHERE 1=1 ".$WHERE_DATE;
+		// echo $qDetail1; exit;
+		$restDetail1	= $this->db->query($sql)->result_array();
+
+		$Row		= 1;
+		$NewRow		= $Row+1;
+		$Col_Akhir	= $Cols	= getColsChar(10);
+		$sheet->setCellValue('A'.$Row, 'REPORT COGS');
+		$sheet->getStyle('A'.$Row.':'.$Col_Akhir.$NewRow)->applyFromArray($mainTitle);
+		$sheet->mergeCells('A'.$Row.':'.$Col_Akhir.$NewRow);
+
+		$NewRow	= $NewRow +2;
+		$NextRow= $NewRow;
+
+		$sheet->setCellValue('A'.$NewRow, '#');
+		$sheet->getStyle('A'.$NewRow.':A'.$NextRow)->applyFromArray($tableHeader);
+		$sheet->mergeCells('A'.$NewRow.':A'.$NextRow);
+		$sheet->getColumnDimension('A')->setWidth(10);
+
+		$sheet->setCellValue('B'.$NewRow, 'TANGGAL');
+		$sheet->getStyle('B'.$NewRow.':B'.$NextRow)->applyFromArray($tableHeader);
+		$sheet->mergeCells('B'.$NewRow.':B'.$NextRow);
+		$sheet->getColumnDimension('B')->setWidth(20);
+
+		$sheet->setCellValue('C'.$NewRow, 'NO SO');
+		$sheet->getStyle('C'.$NewRow.':C'.$NextRow)->applyFromArray($tableHeader);
+		$sheet->mergeCells('C'.$NewRow.':C'.$NextRow);
+		$sheet->getColumnDimension('C')->setAutoSize(true);
+
+		$sheet->setCellValue('D'.$NewRow, 'PRODUCT');
+		$sheet->getStyle('D'.$NewRow.':D'.$NextRow)->applyFromArray($tableHeader);
+		$sheet->mergeCells('D'.$NewRow.':D'.$NextRow);
+		$sheet->getColumnDimension('D')->setAutoSize(true);
+
+		$sheet->setCellValue('E'.$NewRow, 'ID TRANS');
+		$sheet->getStyle('E'.$NewRow.':E'.$NextRow)->applyFromArray($tableHeader);
+		$sheet->mergeCells('E'.$NewRow.':E'.$NextRow);
+		$sheet->getColumnDimension('E')->setWidth(10);
+
+		$sheet->setCellValue('F'.$NewRow, 'NO TRANS');
+		$sheet->getStyle('F'.$NewRow.':F'.$NextRow)->applyFromArray($tableHeader);
+		$sheet->mergeCells('F'.$NewRow.':F'.$NextRow);
+		$sheet->getColumnDimension('F')->setWidth(10);
+		
+		$sheet->setCellValue('G'.$NewRow, 'QTY');
+		$sheet->getStyle('G'.$NewRow.':G'.$NextRow)->applyFromArray($tableHeader);
+		$sheet->mergeCells('G'.$NewRow.':G'.$NextRow);
+		$sheet->getColumnDimension('G')->setWidth(20);
+
+        $sheet->setCellValue('H'.$NewRow, 'NILAI IN CUSTOMER');
+		$sheet->getStyle('H'.$NewRow.':H'.$NextRow)->applyFromArray($tableHeader);
+		$sheet->mergeCells('H'.$NewRow.':H'.$NextRow);
+		$sheet->getColumnDimension('H')->setWidth(20);
+
+        $sheet->setCellValue('I'.$NewRow, 'NO SPK');
+		$sheet->getStyle('I'.$NewRow.':I'.$NextRow)->applyFromArray($tableHeader);
+		$sheet->mergeCells('I'.$NewRow.':I'.$NextRow);
+		$sheet->getColumnDimension('I')->setWidth(20);
+
+		$sheet->setCellValue('J'.$NewRow, 'Material Name');
+		$sheet->getStyle('J'.$NewRow.':J'.$NextRow)->applyFromArray($tableHeader);
+		$sheet->mergeCells('J'.$NewRow.':J'.$NextRow);
+		$sheet->getColumnDimension('J')->setWidth(20);
+
+		$sheet->setCellValue('K'.$NewRow, 'Qty/Berat');
+		$sheet->getStyle('K'.$NewRow.':K'.$NextRow)->applyFromArray($tableHeader);
+		$sheet->mergeCells('K'.$NewRow.':K'.$NextRow);
+		$sheet->getColumnDimension('K')->setWidth(20);
+
+		$sheet->setCellValue('L'.$NewRow, 'Costbook');
+		$sheet->getStyle('L'.$NewRow.':L'.$NextRow)->applyFromArray($tableHeader);
+		$sheet->mergeCells('L'.$NewRow.':L'.$NextRow);
+		$sheet->getColumnDimension('L')->setWidth(20);
+
+		// echo $qDetail1; exit;
+		if($restDetail1){
+			$awal_row	= $NextRow;
+			$no=0;
+			foreach($restDetail1 as $key => $row){
+				$no++;
+				$awal_row++;
+				$awal_col	= 0;
+              
+				$awal_col++;
+				$Cols			= getColsChar($awal_col);
+				$sheet->setCellValue($Cols.$awal_row, $no);
+				$sheet->getStyle($Cols.$awal_row)->applyFromArray($tableBodyLeft);
+
+				$awal_col++;
+				$tanggal	= $row['tanggal'];
+				$Cols			= getColsChar($awal_col);
+				$sheet->setCellValue($Cols.$awal_row, $tanggal);
+				$sheet->getStyle($Cols.$awal_row)->applyFromArray($tableBodyLeft);
+
+				$awal_col++;
+				$no_so	= $row['no_so'];
+				$Cols			= getColsChar($awal_col);
+				$sheet->setCellValue($Cols.$awal_row, $no_so);
+				$sheet->getStyle($Cols.$awal_row)->applyFromArray($tableBodyLeft);
+
+                $awal_col++;
+				$product	= $row['product'];
+				$Cols			= getColsChar($awal_col);
+				$sheet->setCellValue($Cols.$awal_row, $product);
+				$sheet->getStyle($Cols.$awal_row)->applyFromArray($tableBodyLeft);
+
+                $awal_col++;
+				$id_trans	= $row['id_trans'];
+				$Cols			= getColsChar($awal_col);
+				$sheet->setCellValue($Cols.$awal_row, $id_trans);
+				$sheet->getStyle($Cols.$awal_row)->applyFromArray($tableBodyLeft);
+
+				$awal_col++;
+				$kode_trans	= $row['kode_trans'];
+				$Cols			= getColsChar($awal_col);
+				$sheet->setCellValue($Cols.$awal_row, $kode_trans);
+				$sheet->getStyle($Cols.$awal_row)->applyFromArray($tableBodyLeft);
+
+                $awal_col++;
+				$Cols			= getColsChar($awal_col);
+				$sheet->setCellValue($Cols.$awal_row, 1);
+				$sheet->getStyle($Cols.$awal_row)->applyFromArray($tableBodyRight);
+
+				$awal_col++;
+				$nilai_unit	= $row['nilai_unit'];
+				$Cols			= getColsChar($awal_col);
+				$sheet->setCellValue($Cols.$awal_row, $nilai_unit);
+				$sheet->getStyle($Cols.$awal_row)->applyFromArray($tableBodyRight);
+
+				$awal_col++;
+				$no_spk	= $row['no_spk'];
+				$Cols			= getColsChar($awal_col);
+				$sheet->setCellValue($Cols.$awal_row, $no_spk);
+				$sheet->getStyle($Cols.$awal_row)->applyFromArray($tableBodyLeft);
+
+				$awal_col++;
+				$jenis	= $row['nm_material'];
+				$Cols			= getColsChar($awal_col);
+				$sheet->setCellValue($Cols.$awal_row, $jenis);
+				$sheet->getStyle($Cols.$awal_row)->applyFromArray($tableBodyLeft);
+
+				$awal_col++;
+				$QTY = (!empty($row['id_material']))?number_format($row['qty_mat'],4):'';
+				$Cols			= getColsChar($awal_col);
+				$sheet->setCellValue($Cols.$awal_row, $QTY);
+				$sheet->getStyle($Cols.$awal_row)->applyFromArray($tableBodyLeft);
+
+				$awal_col++;
+				$cost_book = (!empty($row['id_material']))?number_format($row['cost_book'],2):'';
+				$Cols			= getColsChar($awal_col);
+				$sheet->setCellValue($Cols.$awal_row, $cost_book);
+				$sheet->getStyle($Cols.$awal_row)->applyFromArray($tableBodyLeft);
+
+			}
+		}
+
+
+		$sheet->setTitle('Report IC');
+		//mulai menyimpan excel format xlsx, kalau ingin xls ganti Excel2007 menjadi Excel5
+		$objWriter		= PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+		ob_end_clean();
+		//sesuaikan headernya
+		header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+		header("Cache-Control: no-store, no-cache, must-revalidate");
+		header("Cache-Control: post-check=0, pre-check=0", false);
+		header("Pragma: no-cache");
+		header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		//ubah nama file saat diunduh
+		header('Content-Disposition: attachment;filename="report-in-customer.xls"');
+		//unduh file
+		$objWriter->save("php://output");
+	}
 
 }
