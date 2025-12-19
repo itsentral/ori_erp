c8fb7ab07f4555715c81c85a3965906ec42f27f5
SamSyamsudin <syams.69@gmail.com>
2025-12-19 10:11:54 +0700

    Update Component.php

--- application/controllers/Component.php
+++ application/controllers/Component.php
@@ -14,7 +14,7 @@ class Component extends CI_Controller {
 			redirect('login');
 		}
 
-		ini_set('memory_limit', '2G');
+		ini_set('memory_limit', '2G'); 
     }
 	
 	//==========================================================================================================================
