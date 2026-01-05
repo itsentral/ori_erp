ca9b426fbab574428fc047d2f5a3a7549a4a8b75
SamSyamsudin <syams.69@gmail.com>
2026-01-05 13:46:50 +0700

    Update Warehouse_stock_tras.php

--- application/controllers/Warehouse_stock_tras.php
+++ application/controllers/Warehouse_stock_tras.php
@@ -983,7 +983,6 @@ class Warehouse_stock_tras extends CI_Controller {
 		$Mulai_Next++;
 		$Mulai_Next2 	= $Mulai_Next + 2;
 		
-		$Col_Name	= getColsChar($Mulai_Next);
 		$Col_Name2	= getColsChar($Mulai_Next2);
 		
 		$sheet->setCellValue($Col_Name.$NextRow, 'Stock');
