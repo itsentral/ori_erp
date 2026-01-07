f2f234a394f9e1df7404d545b18d264a72605ae2
SamSyamsudin <syams.69@gmail.com>
2026-01-07 13:47:22 +0700

    update top PO

--- application/views/Purchase_order/form_receive_invoice.php
+++ application/views/Purchase_order/form_receive_invoice.php
@@ -25,21 +25,21 @@ $this->load->view('include/side_menu');
 				         <label for="matauang" class="control-label">Mata Uang</font></label>
                             <select id="matauang2" name="matauang2" class="form-control"> 
 							
-							<?							
+							<?php							
 							$matauang = $results->matauang_receive_invoice;		
 							if($matauang =='IDR'){?>
 							<option value=''>Pilih Matauang</option>
 							<option value ='IDR' selected >IDR</option>
 							<option value ='USD' >USD</option>	
-							<? }elseif($matauang =='USD'){ ?>
+							<?php }elseif($matauang =='USD'){ ?>
 							<option value=''>Pilih Matauang</option>
 							<option value ='IDR' >IDR</option>
 							<option value ='USD' selected >USD</option>	
-							<? }else{ ?>
+							<?php }else{ ?>
 							<option value=''>Pilih Matauang</option>
 							<option value ='IDR' >IDR</option>
 							<option value ='USD' >USD</option>	
-							<?}?>
+							<?php}?>
 							
 							
 							
