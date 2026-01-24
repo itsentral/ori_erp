4b63d8ea9e7b1441b681c77eb47349920dee3c0d
SamSyamsudin <syams.69@gmail.com>
2026-01-24 16:16:07 +0700

    Update form_top.php

--- application/views/Purchase_order/form_top.php
+++ application/views/Purchase_order/form_top.php
@@ -207,11 +207,7 @@ $kurs=1;
 									echo '<br /><a class="create1 btn btn-xs btn-primary" href="'.base_url('purchase/request_payment/'.$valx['id']).'" title="Request Payment"> <i class="fa fa-money"></i> Request Payment</a>';
 									echo '<br /><a class="receive btn btn-xs btn-success" href="'.base_url('purchase/invoice_receive/'.$valx['id']).'" title="Receive Invoice"> <i class="fa fa-newspaper-o"></i> Receive Invoice</a>';	
 									
-								}elseif($styledisabled=="" && $valx['invoice_no'] !='null' ){
-									
-									echo '<br /><a class="create1 btn btn-xs btn-primary" href="'.base_url('purchase/request_payment/'.$valx['id']).'" title="Request Payment"> <i class="fa fa-money"></i> Request Payment</a>';
-									echo '<br /><a class="receive btn btn-xs btn-success" href="'.base_url('purchase/invoice_receive/'.$valx['id']).'" title="Receive Invoice"> <i class="fa fa-newspaper-o"></i> View Receive Inv</a>';									
-																	
+													
 								} else{
 									echo '<a class="btn btn-xs btn-default" href="'.base_url('purchase/print_request/'.$valx['id']).'" title="Print Request" target="_blank"> <i class="fa fa-print"></i> Print Request</a>';
 								    echo '<br /><a class="receive btn btn-xs btn-success" href="'.base_url('purchase/invoice_receive/'.$valx['id']).'" title="Receive Invoice"> <i class="fa fa-newspaper-o"></i> View Receive Inv</a>';									
