ccb1a6e97eae936ac036320fd00429623c55fc04
SamSyamsudin <syams.69@gmail.com>
2026-01-24 19:27:10 +0700

    Update form_top.php

--- application/views/Purchase_order/form_top.php
+++ application/views/Purchase_order/form_top.php
@@ -208,10 +208,10 @@ $kurs=1;
 									echo '<br /><a class="receive btn btn-xs btn-success" href="'.base_url('purchase/invoice_receive/'.$valx['id']).'" title="Receive Invoice"> <i class="fa fa-newspaper-o"></i> Receive Invoice</a>';	
 									
 													
-								}elseif($styledisabled !="" && $valx['group_top']=='uang muka' && $data[0]->total_terima_barang_idr < 1){
+								}elseif($styledisabled !="" && $valx['group_top']=='uang muka'){
 									echo '<a class="btn btn-xs btn-default" href="'.base_url('purchase/print_request/'.$valx['id']).'" title="Print Request" target="_blank"> <i class="fa fa-print"></i> Print Request</a>';
 								    echo '<br /><a class="receive btn btn-xs btn-success" href="'.base_url('purchase/invoice_receive/'.$valx['id']).'" title="Receive Invoice"> <i class="fa fa-newspaper-o"></i> View Receive Inv</a>';									
-								}elseif($styledisabled !="" && $valx['group_top']=='progress' && $data[0]->total_terima_barang_idr > 0){
+								}elseif($styledisabled !="" && $valx['group_top']=='progress' ){
 									echo '<br /><a class="create1 btn btn-xs btn-primary" href="'.base_url('purchase/request_payment/'.$valx['id']).'" title="Request Payment"> <i class="fa fa-money"></i> Request Payment</a>';
 									echo '<br /><a class="receive btn btn-xs btn-success" href="'.base_url('purchase/invoice_receive/'.$valx['id']).'" title="Receive Invoice"> <i class="fa fa-newspaper-o"></i> View Receive Inv</a>';				
 								}
