14cbcc14b9c04f00d54793afb45f12385fe0e738
arwant2411 <suparwanto4141@gmail.com>
2026-01-25 11:07:39 +0700

    filter dashboard sales

--- application/views/Dashboard/sales.php
+++ application/views/Dashboard/sales.php
@@ -18,100 +18,117 @@ $this->load->view('include/side_menu_dashboard');
 				<div class="col-lg-12">
 					<div class="box box-info">
 						<div class="box-body">
-                                <!-- Sales -->
-                                    <div class="row">
-                                        <div class="col-lg-4">
-                                            <div class="small-box bg-aqua">
-                                                <div class="inner">
-                                                <h3><?= $late_enggenering;?></h3>
-                                                <p>Late Estimation</p>
-                                                </div>
-                                                <div class="icon">
-                                                <i class="ion ion-stats-bars"></i>
-                                                </div>
-                                                <a href="<?php echo site_url('dashboard/print_late_eng') ?>" target='_blank' class="small-box-footer">Print <i class="fa fa-arrow-circle-right"></i></a>
-                                            </div>
+                            <div class="row">
+                                <div class="col-lg-3">
+                                    <p>Filter Tahun</p>
+                                    <select name="filter_tahun" id="filter_tahun" class='form-control'>
+                                        <?php
+                                        for ($i=date('Y'); $i >= 2021; $i--) { 
+                                            $selected = ($i == $year)?'selected':'';
+                                            echo "<option value='".$i."' ".$selected.">".$i."</option>";
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <div class="col-lg-3">
+                                    <p style='color:transparant;'>&nbsp;</p>
+                                    <button type='button' id='filter' class='btn btn-sm btn-primary'>Filter</button>
+                                </div>
+                            </div>
+                            <br>
+                            <!-- Sales -->
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="small-box bg-aqua">
+                                        <div class="inner">
+                                        <h3><?= $late_enggenering;?></h3>
+                                        <p>Late Estimation</p>
                                         </div>
-
-                                        <div class="col-lg-4">
-                                            <div class="small-box bg-green">
-                                                <div class="inner">
-                                                <h3><?= $late_costing;?></h3>
-                                                <p>Late Costing</p>
-                                                </div>
-                                                <div class="icon">
-                                                <i class="ion ion-stats-bars"></i>
-                                                </div>
-                                                <a href="<?php echo site_url('dashboard/print_late_cos') ?>" target='_blank' class="small-box-footer">Print <i class="fa fa-arrow-circle-right"></i></a>
-                                            </div>
+                                        <div class="icon">
+                                        <i class="ion ion-stats-bars"></i>
                                         </div>
-
-                                        <div class="col-lg-4">
-                                            <div class="small-box bg-orange">
-                                                <div class="inner">
-                                                <h3><?= $late_quotation;?></h3>
-                                                <p>Waiting Approval Quotation</p>
-                                                </div>
-                                                <div class="icon">
-                                                <i class="ion ion-stats-bars"></i>
-                                                </div>
-                                                <a href="<?php echo site_url('sales/approve_quotation') ?>" target='_blank' class="small-box-footer">To Link <i class="fa fa-arrow-circle-right"></i></a>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-lg-4">
-                                            <div class="small-box bg-purple">
-                                                <div class="inner">
-                                                <h3>$ <?= number_format($total_quotation,2);?></h3>
-                                                <p>Total Quotation <?=date('Y')?></p>
-                                                </div>
-                                                <div class="icon">
-                                                <i class="ion ion-stats-bars"></i>
-                                                </div>
-                                                <a href="<?php echo site_url('dashboard/print_total_quotation') ?>" target='_blank' class="small-box-footer">Print <i class="fa fa-arrow-circle-right"></i></a>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-lg-4">
-                                            <div class="small-box bg-blue">
-                                                <div class="inner">
-                                                <h3>$ <?= number_format($total_so,2);?></h3>
-                                                <p>Total Sales Order <?=date('Y')?></p>
-                                                </div>
-                                                <div class="icon">
-                                                <i class="ion ion-stats-bars"></i>
-                                                </div>
-                                                <a href="<?php echo site_url('dashboard/print_total_sales_order') ?>" target='_blank' class="small-box-footer">Print <i class="fa fa-arrow-circle-right"></i></a>
-                                            </div>
-                                        </div>
-
-                                        <!-- <div class="col-lg-4">
-                                            <div class="small-box bg-red">
-                                                <div class="inner">
-                                                <h3>Coming Soon </h3>
-                                                <p>Cancel Order</p>
-                                                </div>
-                                                <div class="icon">
-                                                <i class="ion ion-stats-bars"></i>
-                                                </div>
-                                                <a href="<?php echo site_url('dashboard') ?>" target='_blank' class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-                                            </div>
-                                        </div> -->
-                                        <div class="col-lg-4">
-                                            <div class="small-box bg-red">
-                                                <div class="inner">
-                                                <h3><?=number_format($ttl_inv->ttl_inv); ?></h3>
-                                                <p>Jumlah Invoice <?=date("Y")?></p>
-                                                </div>
-                                                <div class="icon">
-                                                <i class="ion ion-stats-bars"></i>
-                                                </div>
-                                                <a href="#()" class="small-box-footer detail">More info <i class="fa fa-arrow-circle-right"></i></a>
-                                            </div>
-                                        </div>
-                                        
+                                        <a href="<?php echo site_url('dashboard/print_late_eng') ?>" target='_blank' class="small-box-footer">Print <i class="fa fa-arrow-circle-right"></i></a>
                                     </div>
+                                </div>
 
+                                <div class="col-lg-4">
+                                    <div class="small-box bg-green">
+                                        <div class="inner">
+                                        <h3><?= $late_costing;?></h3>
+                                        <p>Late Costing</p>
+                                        </div>
+                                        <div class="icon">
+                                        <i class="ion ion-stats-bars"></i>
+                                        </div>
+                                        <a href="<?php echo site_url('dashboard/print_late_cos') ?>" target='_blank' class="small-box-footer">Print <i class="fa fa-arrow-circle-right"></i></a>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-4">
+                                    <div class="small-box bg-orange">
+                                        <div class="inner">
+                                        <h3><?= $late_quotation;?></h3>
+                                        <p>Waiting Approval Quotation</p>
+                                        </div>
+                                        <div class="icon">
+                                        <i class="ion ion-stats-bars"></i>
+                                        </div>
+                                        <a href="<?php echo site_url('sales/approve_quotation') ?>" target='_blank' class="small-box-footer">To Link <i class="fa fa-arrow-circle-right"></i></a>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-4">
+                                    <div class="small-box bg-purple">
+                                        <div class="inner">
+                                        <h3>$ <?= number_format($total_quotation,2);?></h3>
+                                        <p>Total Quotation <?=$year?></p>
+                                        </div>
+                                        <div class="icon">
+                                        <i class="ion ion-stats-bars"></i>
+                                        </div>
+                                        <a href="<?php echo site_url('dashboard/print_total_quotation') ?>" target='_blank' class="small-box-footer">Print <i class="fa fa-arrow-circle-right"></i></a>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-4">
+                                    <div class="small-box bg-blue">
+                                        <div class="inner">
+                                        <h3>$ <?= number_format($total_so,2);?></h3>
+                                        <p>Total Sales Order <?=$year?></p>
+                                        </div>
+                                        <div class="icon">
+                                        <i class="ion ion-stats-bars"></i>
+                                        </div>
+                                        <a href="<?php echo site_url('dashboard/print_total_sales_order') ?>" target='_blank' class="small-box-footer">Print <i class="fa fa-arrow-circle-right"></i></a>
+                                    </div>
+                                </div>
+
+                                <!-- <div class="col-lg-4">
+                                    <div class="small-box bg-red">
+                                        <div class="inner">
+                                        <h3>Coming Soon </h3>
+                                        <p>Cancel Order</p>
+                                        </div>
+                                        <div class="icon">
+                                        <i class="ion ion-stats-bars"></i>
+                                        </div>
+                                        <a href="<?php echo site_url('dashboard') ?>" target='_blank' class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                                    </div>
+                                </div> -->
+                                <div class="col-lg-4">
+                                    <div class="small-box bg-red">
+                                        <div class="inner">
+                                        <h3><?=number_format($ttl_inv->ttl_inv); ?></h3>
+                                        <p>Jumlah Invoice <?=$year?></p>
+                                        </div>
+                                        <div class="icon">
+                                        <i class="ion ion-stats-bars"></i>
+                                        </div>
+                                        <a href="javascript:void(0)" class="small-box-footer detail">More info <i class="fa fa-arrow-circle-right"></i></a>
+                                    </div>
+                                </div>
+                                
+                            </div>
 						</div>
 					</div>
 				</div>
@@ -129,7 +146,7 @@ $this->load->view('include/side_menu_dashboard');
 		$(".modal-title").html("<b>LIST INVOICE</b>");
 		$.ajax({
 			type:'POST',
-			url: base_url + active_controller+'/list_invoice_dash/',
+			url: base_url + active_controller+'/list_invoice_dash/'+<?=$year;?>,
 			success:function(data){
 				$("#Mymodal").modal();
 				$("#listCoa").html(data);
@@ -148,4 +165,10 @@ $this->load->view('include/side_menu_dashboard');
 			}
 		});
 	});
+
+    $(document).on('click','#filter',function(){
+        let filter_tahun = $('#filter_tahun').val()
+
+        window.location.href = base_url + active_controller+'/index/'+filter_tahun;
+    })
 </script>
