fbfcb337c38cc492c3ca98c1ee15cd1de7e8afb8
SamSyamsudin <syams.69@gmail.com>
2026-01-25 21:54:13 +0700

    Update modal_edit_purchase_order.php

--- application/views/Purchase_order/modal_edit_purchase_order.php
+++ application/views/Purchase_order/modal_edit_purchase_order.php
@@ -70,7 +70,8 @@
 		<div class='col-sm-3'>
 			<?php
 			$buyer = (!empty($data[0]->buyer))?strtoupper($data[0]->buyer):strtoupper(get_name('users','nm_lengkap','username',$data[0]->updated_by));
-			 echo form_input(array('id'=>'buyer','name'=>'buyer','class'=>'form-control input-md','placeholder'=>'Buyer'), $buyer readonly);
+			 echo form_input(array('id'=>'buyer','name'=>'buyer','class'=>'form-control input-md','placeholder'=>'Buyer',
+        'readonly' => 'readonly'), $buyer);
 			?>
 		</div>
 		<label class='label-control col-sm-1'><b>Currency Amount Word</b></label>
