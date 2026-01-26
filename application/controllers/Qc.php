cec4bbf3e954c6684ef248450f5cb7848409fa43
SamSyamsudin <syams.69@gmail.com>
2026-01-26 10:50:19 +0700

    Update Qc.php

--- application/controllers/Qc.php
+++ application/controllers/Qc.php
@@ -4532,10 +4532,11 @@ class Qc extends CI_Controller
 					 $nospk      = $data->no_spk;
 					 $qty        = $data->qty;
 				
-					$this->db->query("UPDATE  warehouse_stock_wipx SET qty = qty-$qty  WHERE no_so ='".$noso."' AND kode_trans ='".$kode_trans."'  AND no_spk ='".$nospk."' AND product ='".$nm_material."'");
-				
+					
 			}
 			
+			$this->db->query("UPDATE  warehouse_stock_wip SET qty = qty-$qty  WHERE no_so ='".$noso."' AND kode_trans ='".$kode_trans."'  AND no_spk ='".$nospk."' AND product ='".$nm_material."'");
+				
 			        
 				
 			
