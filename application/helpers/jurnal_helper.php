d76d565a2ee91f6f37a9345b461051bf6ba393d9
SamSyamsudin <syams.69@gmail.com>
2026-02-06 17:53:40 +0700

    Update jurnal_helper.php

--- application/helpers/jurnal_helper.php
+++ application/helpers/jurnal_helper.php
@@ -3263,9 +3263,7 @@
 		   
 			$jurnalfg = $CI->db->query("SELECT tanggal,keterangan,product,no_so,no_spk,id_trans,kode_trans, nilai_wip as wip, material as material, wip_direct as wip_direct, wip_indirect as wip_indirect,  wip_foh as wip_foh, wip_consumable as wip_consumable, nilai_unit as finishgood  FROM data_erp_fg WHERE id_pro_det ='".$kode."' AND created_date='".$DateTime."' AND jenis LIKE 'out%'")->result();
 			
-			print_r($jurnalfg);
-			exit;
-			  
+					  
 			foreach($jurnalfg AS $data){
 				
 				$nm_material = $data->product;	
