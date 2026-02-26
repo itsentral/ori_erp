5f5758caaf4ab8d5bd90e4325eee7b9ddfee0440
SamSyamsudin <syams.69@gmail.com>
2026-02-26 09:17:22 +0700

    Update Total_value_product.php

--- application/controllers/Total_value_product.php
+++ application/controllers/Total_value_product.php
@@ -201,8 +201,8 @@ class Total_value_product extends CI_Controller {
 			$nestedData[]	= "<div align='left'>".strtoupper($row['product'])."</div>";
 			$nestedData[]	= "<div align='left'>".strtoupper($row['keterangan'])."</div>";
 			$nestedData[]	= "<div align='right'>".number_format($row['qty'],4)."</div>";
+			$nestedData[]	= "<div align='right'>".number_format($row['nilai_wip']/$row['qty'],2)."</div>";
 			$nestedData[]	= "<div align='right'>".number_format($row['nilai_wip'],2)."</div>";
-			$nestedData[]	= "<div align='right'>".number_format($row['nilai_wip']*$row['qty'],2)."</div>";
 			$nestedData[]	= "<div align='center'>
 								<button type='button' class='btn btn-sm btn-warning look_history' title='History' data-no_so='".strtoupper($row['no_so'])."' data-no_spk='".$row['no_spk']."' data-product='".$row['product']."' data-kode_trans='".$row['kode_trans']."'><i class='fa fa-history'></i></button>
 								</div>";
