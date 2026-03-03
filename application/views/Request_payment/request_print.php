8d6b198eec48f8981ab65bd7d4edca5d52b497cd
SamSyamsudin <syams.69@gmail.com>
2026-03-03 15:06:22 +0700

    Update request_print.php

--- application/views/Request_payment/request_print.php
+++ application/views/Request_payment/request_print.php
@@ -77,31 +77,124 @@ if(!empty($data_request)){
 
 	<table valign="top" width="800" border=1 cellpadding=1 cellspacing=0>
 		<tr>
-			<th width="5">#</th>
-			<th>Tanggal</th>
-			<th>Deskripsi</th>
-			<th>Qty</th>
-			<th>Harga</th>
-			<th>Total Harga</th>
-			<th>Keterangan</th>
+		<th nowrap>No</th>
+		<th nowrap>Tgl Pengajuan</th>
+		<th nowrap>COA</th>
+		<th nowrap>Nama Barang</th>
+		<th nowrap>Spesifikasi</th>
+		<th nowrap>Jml</th>
+		<th nowrap>Tgl Dibutuhkan</th>
+		<th nowrap>Biaya Satuan</th>
+		<th nowrap>Total Biaya</th>
+		<th nowrap>Kasbon</th>
+		</tr>
+		<?php $total_expense=0; $total_tol=0;$total_parkir=0;$total_kasbon=0; $idd=1; $total_km=0; $grand_total=0;$i=0;$gambar="";
+		if(!empty($data_detail)){
+			foreach($data_detail AS $record){ $i++;?>
+			<tr>
+				<td valign="top"><?=$i;?></td>
+				<td valign="top"><?=tgl_indo($data->tgl_doc);?></td>
+				<td valign="top"><?php
+				echo $record->coa;
+				$dtcoa=$this->db->query("SELECT * FROM ".DBACC.".coa_master where no_perkiraan='".$record->coa."'")->row();
+				if(!empty($dtcoa)) echo " ".$dtcoa->nama;
+				?></td>
+				<td valign="top"><?=$record->deskripsi;?></td>
+				<td valign="top"><?=$record->keterangan;?></td>
+				<td valign="top" align="right"><?=number_format($record->qty);?></td>
+				<td valign="top"><?=tgl_indo($record->tanggal);?></td>
+				<td valign="top" align="right"><?=number_format($record->harga);?></td>
+				<td valign="top" align="right"><?=number_format($record->expense);?></td>
+				<td valign="top" align="right"><?=number_format($record->kasbon);?></td>
+			</tr>
+			<?php
+				if($record->doc_file!=''){
+					if(strpos($record->doc_file,'pdf',0)>1){
+					}else{
+						$gambar.='<img src="'.base_url("assets/expense/".$record->doc_file).'" width="500"><br />';
+					}
+				}
+				$total_expense=($total_expense+($record->expense));
+				$total_kasbon=($total_kasbon+($record->kasbon));
+				$idd++;
+			}
+		}
+		$grand_total=($total_expense);
+		for($x=0;$x<(5-$i);$x++){
+		echo '
+			<tr>
+				<td>&nbsp;</td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		';
+		}
+		$text="Saldo";$sisakurang="";
+		$all_total=(($total_expense+$data->add_ppn_nilai-$data->add_pph_nilai-$total_kasbon));
+		if ($total_expense>$total_kasbon){
+	//		$text="(Kurang)";
+			$sisakurang=number_format(($total_expense+$data->add_ppn_nilai)-$total_kasbon);
+		}else{
+	//		$text="Sisa Lebih";
+			$sisakurang="( ".number_format(($total_expense+$data->add_ppn_nilai)-$total_kasbon)." )";
+		}
+	?>
+		<tr>
+			<td colspan=3></td>
+			<td colspan=5><strong>Sub Total</strong></td>
+			<td align="right"><?=number_format($total_expense);?></td>
+			<td align="right"><?=number_format($total_kasbon);?></td>
 		</tr>
 	<?php
-	$i=0;
-	if(!empty($data_detail)){
-		foreach($data_detail AS $rec){ $i++;?>
+	if ($data->add_ppn_nilai>0){
+		?>
 		<tr>
-			<td><?=$i;?></td>
-			<td><?= $rec->tanggal ?></td>
-			<td><?= $rec->deskripsi ?></td>
-			<td align=right><?= number_format($rec->qty) ?></td>
-			<td align=right><?= number_format($rec->harga) ?></td>
-			<td align=right><?= number_format($rec->total_harga) ?></td>
-			<td><?= $rec->keterangan ?></td>
+			<td colspan=3></td>
+			<td><strong>PPN</strong></td>
+			<td colspan=4><?php
+				echo $data->add_ppn_coa;
+				$dtcoa=$this->db->query("SELECT * FROM ".DBACC.".coa_master where no_perkiraan='".$data->add_ppn_coa."'")->row();
+				if(!empty($dtcoa)) echo " ".$dtcoa->nama;
+				?></td>
+			<td colspan=2 align=right><?=number_format($data->add_ppn_nilai);?></td>
 		</tr>
-		<?php
-		}
+	<?php }
+	if ($data->add_pph_nilai>0){ ?>
+		<tr>
+			<td colspan=3></td>
+			<td><strong>PPH</strong></td>
+			<td colspan=4><?php
+				echo $data->add_pph_coa;
+				$dtcoa=$this->db->query("SELECT * FROM ".DBACC.".coa_master where no_perkiraan='".$data->add_pph_coa."'")->row();
+				if(!empty($dtcoa)) echo " ".$dtcoa->nama;
+				?></td>
+			<td colspan=2 align=right><?=number_format($data->add_pph_nilai);?></td>
+		</tr>
+	<?php }
+	if($all_total>0) { ?>
+		<tr>
+			<td colspan=3></td>
+			<td colspan=5><strong>Total</strong></td>
+			<td colspan=2 align=right><?=number_format($all_total);?></td>
+		</tr>
+		<?php 
 	}
-	?>
+	if($total_kasbon>0) {?>
+		<tr>
+			<td colspan=8 align=center><strong><?=$text?></strong></td>
+			<td colspan=2 align="right"><?=$sisakurang;?></td>
+		</tr>
+		<?php } ?>
+		</table><br />
+		</td>
+	</tr>
 	</table>
 
 	
