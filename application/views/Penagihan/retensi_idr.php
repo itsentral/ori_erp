4f3749d4955d00f217b4576f9638bf798aed6a83
SamSyamsudin <syams.69@gmail.com>
2026-04-14 09:56:38 +0700

    Update retensi_idr.php

--- application/views/Penagihan/retensi_idr.php
+++ application/views/Penagihan/retensi_idr.php
@@ -20,11 +20,6 @@
 						$nomor=0;
 						foreach($get_retensi AS $val => $valx){ 
 							$nomor++;
-
-							print_r($approval);
-							exit;
-
-
 							if(!empty($approval)){
 							$harga_tot6			= number_format($valx['harga_total_idr'],2);
 							$harga_tot6_hidden	= round($valx['harga_total_idr'],2);
