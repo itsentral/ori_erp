bb6a9b30baf6aab92298d1fe5f46dfb2526f3359
SamSyamsudin <syams.69@gmail.com>
2026-07-20 16:46:55 +0700

    Update print_pdf.php

--- application/views/Serah_terima_asset/print_pdf.php
+++ application/views/Serah_terima_asset/print_pdf.php
@@ -10,67 +10,106 @@ $idt = group_company();
         body {
             font-family: Arial, sans-serif;
             font-size: 12px;
-            margin: 20px;
+            margin: 30px 40px;
         }
-        .header-title {
-            text-align: center;
-            margin-bottom: 20px;
-        }
-        .header-title h2 {
-            margin: 0;
-            padding: 5px 0;
-        }
-        .header-title h3 {
-            margin: 0;
-            padding: 5px 0;
-            font-weight: normal;
-        }
-        .info-table {
+        .header-table {
             width: 100%;
+            border-collapse: collapse;
             margin-bottom: 20px;
         }
-        .info-table td {
-            padding: 4px 8px;
+        .header-table td {
+            vertical-align: middle;
+            padding: 5px;
+        }
+        .header-table .logo {
+            width: 80px;
+            border: 1px solid #333;
+            text-align: center;
+            padding: 10px;
+        }
+        .header-table .title-cell {
+            text-align: center;
+            font-size: 16px;
+            font-weight: bold;
+            border: 1px solid #333;
+        }
+        .header-table .doc-info {
+            border: 1px solid #333;
+            font-size: 11px;
+            padding: 5px 10px;
+        }
+        .header-table .doc-info table td {
+            padding: 2px 5px;
+            border: none;
+        }
+        .info-section {
+            width: 100%;
+            margin-bottom: 15px;
+        }
+        .info-section td {
+            padding: 3px 0;
             vertical-align: top;
         }
         .info-label {
-            font-weight: bold;
-            width: 120px;
+            width: 100px;
+        }
+        .checkbox-section {
+            margin-bottom: 15px;
+        }
+        .checkbox-section span {
+            margin-right: 80px;
         }
         .detail-table {
             width: 100%;
             border-collapse: collapse;
-            margin-bottom: 30px;
+            margin-bottom: 20px;
         }
         .detail-table th, .detail-table td {
             border: 1px solid #333;
-            padding: 6px 8px;
-            text-align: left;
+            padding: 5px 8px;
+            vertical-align: top;
         }
         .detail-table th {
-            background-color: #f0f0f0;
+            background-color: #f5f5f5;
             text-align: center;
             font-weight: bold;
         }
         .detail-table td.center {
             text-align: center;
         }
+        .note-section {
+            margin-top: 10px;
+            margin-bottom: 20px;
+            font-size: 11px;
+        }
         .signature-table {
             width: 100%;
-            margin-top: 40px;
+            margin-top: 30px;
         }
         .signature-table td {
             text-align: center;
-            padding: 10px;
+            padding: 10px 5px;
             vertical-align: top;
+            width: 25%;
         }
-        .signature-line {
-            border-bottom: 1px solid #333;
-            width: 150px;
-            margin: 60px auto 5px auto;
+        .signature-space {
+            height: 60px;
+        }
+        .note-box {
+            border: 1px solid #333;
+            padding: 10px;
+            margin-top: 15px;
+            font-size: 11px;
+        }
+        .note-box ul {
+            margin: 5px 0;
+            padding-left: 20px;
+        }
+        .note-box li {
+            margin-bottom: 3px;
         }
         @media print {
-            body { margin: 0; }
+            body { margin: 15px 20px; }
             .no-print { display: none; }
         }
     </style>
@@ -85,77 +124,142 @@ $idt = group_company();
         </button>
     </div>
 
-    <div class="header-title">
-        <h2><?= $idt->nm_perusahaan; ?></h2>
-        <h3>FORM SERAH TERIMA ASSET</h3>
-    </div>
-
-    <table class="info-table">
+    <!-- HEADER -->
+    <table class="header-table">
         <tr>
-            <td class="info-label">Form No.</td>
-            <td>: <?= $data['form_no']; ?></td>
-            <td class="info-label">Lokasi</td>
-            <td>: <?= $data['lokasi']; ?></td>
-        </tr>
-        <tr>
-            <td class="info-label">Tanggal</td>
-            <td>: <?= date('d F Y', strtotime($data['created_date'])); ?></td>
-            <td class="info-label">Departemen</td>
-            <td>: <?= $data['nm_dept']; ?></td>
-        </tr>
-        <tr>
-            <td class="info-label">Sender</td>
-            <td>: <?= $data['sender']; ?></td>
-            <td class="info-label">Receiver</td>
-            <td>: <?= $data['receiver']; ?></td>
+            <td class="logo" width="80">
+                <img src="<?= base_url('assets/img/logo.png'); ?>" alt="Logo" style="max-width:60px; max-height:50px;">
+            </td>
+            <td class="title-cell">FORM SERAH TERIMA ASSET & TOOLS</td>
+            <td class="doc-info" width="180">
+                <table>
+                    <tr><td>Document No.</td><td>: FM-S2.2-12</td></tr>
+                    <tr><td>Rev. No.</td><td>: 1</td></tr>
+                    <tr><td>Issue Date</td><td>: 11 Sept 2024</td></tr>
+                </table>
+            </td>
         </tr>
     </table>
 
+    <!-- INFO SECTION -->
+    <table class="info-section">
+        <tr>
+            <td class="info-label">Nama</td>
+            <td width="5">:</td>
+            <td><?= $data['receiver']; ?></td>
+            <td width="100" style="text-align:right;">Tanggal</td>
+            <td width="5" style="text-align:center;">:</td>
+            <td width="150"><?= date('d F Y', strtotime($data['created_date'])); ?></td>
+        </tr>
+        <tr>
+            <td class="info-label">Lokasi</td>
+            <td>:</td>
+            <td><?= $data['lokasi']; ?></td>
+            <td></td><td></td><td></td>
+        </tr>
+        <tr>
+            <td class="info-label">Department</td>
+            <td>:</td>
+            <td><?= $data['nm_dept']; ?></td>
+            <td></td><td></td><td></td>
+        </tr>
+    </table>
+
+    <!-- CHECKBOX SECTION -->
+    <div class="checkbox-section">
+        <p>Telah dilakukan,</p>
+        <span>&#9744; Penerimaan Asset / Tools*</span>
+        <span>&#9744; Pemasangan Sparepart / Komponen</span>
+        <p>Dengan jumlah dan spesifikasi sebagai berikut :</p>
+    </div>
+
+    <!-- DETAIL TABLE -->
     <table class="detail-table">
         <thead>
             <tr>
                 <th width="5%">No</th>
-                <th>Asset Name</th>
-                <th>Assets Code</th>
-                <th>New Assets Code</th>
-                <th>Model</th>
-                <th>Merk</th>
+                <th>Deskripsi Asset / Tools</th>
+                <th width="12%">TTB</th>
+                <th width="8%">QTY</th>
+                <th width="15%">KODE ASSET</th>
             </tr>
         </thead>
         <tbody>
             <?php if(!empty($detail)): ?>
                 <?php foreach($detail as $idx => $det): ?>
                 <tr>
-                    <td class="center"><?= $idx + 1; ?></td>
-                    <td><?= $det['nm_asset']; ?></td>
-                    <td><?= $det['assets_code']; ?></td>
+                    <td class="center" rowspan="4"><?= $idx + 1; ?></td>
+                    <td><b>Nama Asset / Tools :</b> <?= $det['nm_asset']; ?></td>
+                    <td></td>
+                    <td></td>
                     <td><?= $det['new_assets_code']; ?></td>
-                    <td><?= $det['model']; ?></td>
-                    <td><?= $det['merk']; ?></td>
+                </tr>
+                <tr>
+                    <td><b>Model</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <?= $det['model']; ?></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><b>Merk</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <?= $det['merk']; ?></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><b>Kode Asset Utama / Kode Asset lama* :</b> <?= $det['assets_code']; ?></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
                 <?php endforeach; ?>
             <?php endif; ?>
+            <tr>
+                <td colspan="2" class="center"><b>Total</b></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
         </tbody>
     </table>
 
+    <!-- DISCLAIMER -->
+    <div class="note-section">
+        <p>*Asset/Tools/Sparepart/komponen tersebut telah kami terima/pasangkan dalam kondisi baik dan dapat digunakan untuk kegiatan operasional perusahaan.<br>
+        <i>*coret yang tidak perlu</i></p>
+    </div>
+
+    <!-- SIGNATURE -->
     <table class="signature-table">
         <tr>
-            <td>
-                <b>Yang Menyerahkan</b><br>(Sender)
-                <div class="signature-line"></div>
-                <b><?= $data['sender']; ?></b>
-            </td>
-            <td>
-                <b>Yang Menerima</b><br>(Receiver)
-                <div class="signature-line"></div>
-                <b><?= $data['receiver']; ?></b>
-            </td>
-            <td>
-                <b>Mengetahui</b><br>(Atasan)
-                <div class="signature-line"></div>
-                ___________________
-            </td>
+            <td>Diserahkan Oleh</td>
+            <td>Diterima Oleh</td>
+            <td>Disetujui Oleh</td>
+            <td>Diketahui Oleh</td>
+        </tr>
+        <tr>
+            <td class="signature-space"></td>
+            <td class="signature-space"></td>
+            <td class="signature-space"></td>
+            <td class="signature-space"></td>
+        </tr>
+        <tr>
+            <td>( Bag Gudang )</td>
+            <td>( User )</td>
+            <td>( User Dept Head )</td>
+            <td>( Fixed Asset )</td>
         </tr>
     </table>
+    <p style="font-size:10px;"><i>Wajib dilengkapi dengan nama jelas</i></p>
+
+    <!-- NOTE BOX -->
+    <div class="note-box">
+        <b>Note :</b>
+        <ul>
+            <li>Pihak Penerima Asset Harus bertanggung Jawab sepenuhnya untuk menjaga Asset & Tools ini sebagaimana mestinya dan sesuai dengan masa manfaat dari Asset tsb.</li>
+            <li>Form ini diprintout Rangkap 3 (1. Asli bag. Fixed Asset ; 2. User Asset ; 3. Pihak Gudang )</li>
+            <li>Dilampirkan juga copy surat jalan lalu dikirim ke Fixed Asset.</li>
+        </ul>
+    </div>
 </body>
 </html>
