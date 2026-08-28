2b48474e0b8c254838647487db8a35d00724af23
SamSyamsudin <syams.69@gmail.com>
2026-08-28 17:12:47 +0700

    Update index.php

--- application/views/Stock_opname_generate/index.php
+++ application/views/Stock_opname_generate/index.php
@@ -27,6 +27,28 @@
 				</div>
 			</div>
 		</div>
+
+		<!-- Panel Adjust Harga Inventory Produksi -->
+		<div class="row" style="margin-bottom:15px; border-top:1px solid #ddd; padding-top:15px;">
+			<div class="col-sm-3">
+				<label>Tanggal Adjust:</label>
+				<input type="text" id="date_adjust" name="date_adjust" class="form-control text-center" placeholder="Pilih Tanggal">
+			</div>
+			<div class="col-sm-3">
+				<label>Total Inventory (Input):</label>
+				<input type="number" id="total_inventory_input" name="total_inventory_input" class="form-control" placeholder="Masukkan total inventory" step="0.01">
+			</div>
+			<div class="col-sm-3" style="padding-top:25px;">
+				<button type="button" class="btn btn-primary btn-sm" id="btn_adjust_harga">
+					<i class="fa fa-refresh"></i> Adjust Harga Produksi
+				</button>
+			</div>
+			<div class="col-sm-3" style="padding-top:25px;">
+				<div id="info_panel_adjust" class="alert alert-info" style="display:none; padding:8px; margin:0;">
+					<span id="info_text_adjust"></span>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 
@@ -147,6 +169,56 @@ $(document).ready(function(){
 			},
 			error: function(){
 				btn.prop('disabled', false).html('<i class="fa fa-wrench"></i> Fix Tran Detail');
+				alert('Terjadi kesalahan server!');
+			}
+		});
+	});
+
+	// Init datepicker adjust
+	$('#date_adjust').datepicker({
+		dateFormat: 'yy-mm-dd',
+		changeMonth: true,
+		changeYear: true
+	});
+
+	// Adjust Harga Inventory Produksi button
+	$('#btn_adjust_harga').on('click', function(){
+		var date_adjust = $('#date_adjust').val();
+		var total_input = $('#total_inventory_input').val();
+
+		if(!date_adjust){
+			alert('Pilih tanggal adjust terlebih dahulu!');
+			return;
+		}
+		if(!total_input || parseFloat(total_input) <= 0){
+			alert('Total inventory harus diisi dan lebih dari 0!');
+			return;
+		}
+
+		if(!confirm('Adjust harga di tran_warehouse_jurnal_detail gudang produksi tanggal '+date_adjust+'?\n\nTotal inventory target: '+Number(total_input).toLocaleString('id-ID')+'\n\nRumus: harga_baru = (total_inventory / total_input) * harga_lama')){
+			return;
+		}
+
+		var btn = $(this);
+		btn.prop('disabled', true).html('<i class="fa fa-spinner fa-spin"></i> Processing...');
+
+		$.ajax({
+			url: '<?php echo site_url("stock_opname_generate/adjust_harga_inventory"); ?>',
+			type: 'POST',
+			data: { date_target: date_adjust, total_inventory_input: total_input },
+			dataType: 'json',
+			success: function(res){
+				btn.prop('disabled', false).html('<i class="fa fa-refresh"></i> Adjust Harga Produksi');
+				if(res.status == 1){
+					$('#info_panel_adjust').removeClass('alert-danger').addClass('alert-info').show();
+					$('#info_text_adjust').html('<i class="fa fa-check"></i> '+res.pesan);
+				} else {
+					$('#info_panel_adjust').removeClass('alert-info').addClass('alert-danger').show();
+					$('#info_text_adjust').html('<i class="fa fa-warning"></i> '+res.pesan);
+				}
+			},
+			error: function(){
+				btn.prop('disabled', false).html('<i class="fa fa-refresh"></i> Adjust Harga Produksi');
 				alert('Terjadi kesalahan server!');
 			}
 		});
